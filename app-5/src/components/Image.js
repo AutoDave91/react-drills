841afb45bab28c06fd388a47bbc3cec143a3238c
david <davidwnewman91@gmail.com>
2019-05-14 15:20:50 -0500

    Done

---
+++ app-5/src/components/Image.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Image extends Component{
+    render(){
+        return(
+            <main>
+                <img src={'https://via.placeholder.com/150'}  alt='What the Heck mann!'/>
+                <caption>Placeholder!!</caption>
+            </main>
+        )
+    }
+}
+
+export default Image
